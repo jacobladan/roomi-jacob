f98993f65ed8cffac99c5f8bfefdfa01d2b76bef
Denald Demirxhiu <denalddemirxhiu@gmail.com>
2018-12-14 20:30:41 -0500

    Fixed a small bug

--- roomi/app/src/main/java/com/example/roomi/roomi/ChangeEmail.java
+++ roomi/app/src/main/java/com/example/roomi/roomi/ChangeEmail.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -22,10 +24,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class ChangeEmail extends AppCompatActivity {
 
-    private EditText emailEditText, reEnterEmailEditText;
+    private EditText emailEditText, passwordEditText, newEmailEditText, reEnterEmailEditText;
     private Button submitButton;
     private FirebaseUser user;
     private ProgressBar progressBar;
+    private AuthCredential credential;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +50,23 @@ public class ChangeEmail extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Utilities.hideKeyboard(ChangeEmail.this);
-                final String email = emailEditText.getText().toString().trim();
+                String oldEmail = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString();
+                final String newEmail = newEmailEditText.getText().toString().trim();
                 String confirmEmail = reEnterEmailEditText.getText().toString().trim();
 
-                if (email.isEmpty()) {
-                    emailEditText.setError("Enter an email");
+                if (oldEmail.isEmpty()) {
+                    emailEditText.setError("Enter your email");
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    passwordEditText.setError("Enter your password");
+                    return;
+                }
+
+                if (newEmail.isEmpty()) {
+                    newEmailEditText.setError("Enter a new email");
                     return;
                 }
 
@@ -60,29 +75,42 @@ public class ChangeEmail extends AppCompatActivity {
                     return;
                 }
 
-                if (!email.equals(confirmEmail)) {
+                if (oldEmail.equals(newEmail)) {
+                    Toast.makeText(getApplicationContext(), "Old email and new email must not match!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (!newEmail.equals(confirmEmail)) {
                     Toast.makeText(getApplicationContext(), "Emails must match!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
 
-                user.updateEmail(email)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    updateEmail(email);
-                                    Toast.makeText(getApplicationContext(), "Email address was updated.", Toast.LENGTH_LONG).show();
-                                    finish();
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Failed to update email!", Toast.LENGTH_LONG).show();
-                                    return;
-                                }
-                            }
-                        });
+                credential = EmailAuthProvider.getCredential(oldEmail, password);
 
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if(task.isSuccessful()) {
+                            user.updateEmail(newEmail)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            progressBar.setVisibility(View.GONE);
+                                            if (task.isSuccessful()) {
+                                                updateEmail(newEmail);
+                                                Toast.makeText(getApplicationContext(), "Email address was updated.", Toast.LENGTH_LONG).show();
+                                                finish();
+                                            }
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Something went wrong!\nCheck your credentials.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
@@ -109,6 +137,8 @@ public class ChangeEmail extends AppCompatActivity {
 
     private void findViews() {
         emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        newEmailEditText = findViewById(R.id.newEmailEditText);
         reEnterEmailEditText = findViewById(R.id.reEnterEmailEditText);
         submitButton = findViewById(R.id.submit);
         progressBar = findViewById(R.id.progressBar);
